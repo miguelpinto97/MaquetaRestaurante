85ec2b9f6b689a78b24489305f5fb5c06fb564a0
miguelpinto97 <78398370+miguelpinto97@users.noreply.github.com>
2021-11-04 09:47:05 -0500

    Add files via upload

---
+++ js/Menu.js
@@ -0,0 +1,71 @@
+var DiasSemana = [
+   {
+      "Valor": "0",
+      "Dia": "Todos"
+   },
+   {
+      "Valor": "Lunes",
+      "Dia": "Lunes"
+   },
+   {
+      "Valor": "Martes",
+      "Dia": "Martes"
+   },
+   {
+      "Valor": "Miercoles",
+      "Dia": "Miercoles"
+   },
+   {
+      "Valor": "Jueves",
+      "Dia": "Jueves"
+   },
+   {
+      "Valor": "Viernes",
+      "Dia": "Viernes"
+   },
+   {
+      "Valor": "Sabado",
+      "Dia": "Sabado"
+   }
+];
+
+var Categorias = [
+   {
+      "Curso":"Todos los Cursos",
+      "CursoCorto":"0",
+      "DiasTipos":"LunesPresencial MartesPresencial MiercolesPresencial JuevesPresencial ViernesPresencial SabadoPresencial",
+      "Dias":"Lunes Martes Miercoles Jueves Viernes Sabado",
+      "Tipos":"Presencial"
+   },
+   { "Curso": "Criollo", "CursoCorto": "Criollo", "DiasTipos": "MartesVirtual", "Dias": "Martes", "Tipos": "Virtual" },
+   { "Curso": "Marino", "CursoCorto": "Marino", "DiasTipos": "MartesVirtual MiercolesVirtual", "Dias": "Martes Miercoles", "Tipos": "Virtual" }
+];
+
+
+var Menu = [
+   {
+      "NombrePlato":"Arroz con Pollo",
+      "Categoria":"Criollo",
+      "NombreImagen":"veronica",
+      "Precio":"30.00"
+   },
+   {
+      "NombrePlato":"Ají de Gallina",
+      "Categoria":"Criollo",
+      "NombreImagen":"veronica",
+      "Precio":"25.00"
+   },
+   {
+      "NombrePlato":"Ceviche",
+      "Categoria":"Marino",
+      "NombreImagen":"veronica",
+      "Precio":"30.00"
+   },
+   {
+      "NombrePlato":"Trío Marino",
+      "Categoria":"Marino",
+      "NombreImagen":"veronica",
+      "Precio":"25.00"
+   }
+];
+   
---
+++ js/app.js
@@ -0,0 +1,145 @@
+
+
+ko.applyBindings(Menu, document.getElementById('ListaHorarios'));
+ko.applyBindings(Categorias, document.getElementById('ListaCursos'));
+ko.applyBindings(DiasSemana, document.getElementById('selector'));
+
+var MenuFiltrado;
+
+function filtrarMenuxCategoria(categoria) {
+    MenuFiltrado = Menu.filter(function (element) { return element.Categoria == 'Criollo' });
+}
+
+var contador = 0;
+function cambiarFormato() {
+    var elements = document.getElementsByClassName("platoMenu");
+
+    for (let element of elements) {
+        if (contador == 0) {
+            element.className = "formatoLista platoMenu col-11 col-md-9 col-sm-9 m-3 text-center card";
+        } else {
+            element.className = "platoMenu col-5 col-md-3 col-sm-9 m-3 text-center card";
+        }
+    }
+    contador = contador == 0 ? 1 : 0;
+}
+
+var valorDia = '0';
+var valorCurso = '0';
+var valorSwitch = false;
+
+function filtroTotal(vDia, vCurso, vSwitch) {
+
+    var claseDia = '';
+    var claseTipo = '';
+    var claseDiaTipo = '';
+    var claseCurso = '';
+
+    if (vDia != '-1') {
+        valorDia = vDia;
+        valorCurso = '0';
+        limpiarSeleccionCurso();
+        actualizarSeleccionMenuDias();
+    } else if (vCurso != '-1') {
+        valorCurso = vCurso;
+        actualizarSeleccionCurso();
+    } else if (vSwitch != '-1') {
+        valorSwitch = !valorSwitch;
+
+        document.getElementById('textoFiltro').innerHTML = valorSwitch == true ? 'Filtro Activado' : 'Filtro Desactivado';
+
+    }
+
+    claseDia = valorDia == '0' ? '' : '.' + valorDia;
+    claseTipo = valorSwitch == false ? '' : '.Presencial';
+
+    if (valorDia != 0 && valorSwitch == true) {
+        claseDiaTipo = '.' + valorDia + 'Presencial';
+    }
+
+    if (vCurso == '-1') {
+        OcultarMostrarTransicion('#ListaCursos', '0');
+
+        OcultarMostrarTransicion('#ListaCursos', '1');
+
+        OcultarMostrar('.cursoDisponible', 'none');
+
+        OcultarMostrar('.cursoDisponible' + claseDia + claseTipo + claseDiaTipo, 'block');
+    }
+
+    claseCurso = valorCurso == '0' ? '' : '.' + valorCurso;
+    claseTipo = valorSwitch == false ? '' : '.Presencialx';
+
+
+    OcultarMostrarTransicion('.platoMenu', '0');
+    OcultarMostrarTransicion('.platoMenu' + claseDia + claseCurso + claseTipo, '1');
+
+    if (vCurso != '-1') {
+        window.location.href = "index.html#ListaHorarios";
+    }
+}
+
+
+
+function OcultarMostrarTransicion(selector, tipoDisplay) {
+
+    if (tipoDisplay == '0') {
+        $(selector).fadeOut();
+
+    } else {
+        $(selector).fadeIn();
+    }
+}
+
+function OcultarMostrar(selector, tipoDisplay) {
+
+    if (tipoDisplay == 'none') {
+        $(selector).hide();
+
+    } else {
+        $(selector).show();
+    }
+}
+
+
+function asignarClass(id, value) {
+    document.getElementById(id).className = value;
+}
+
+
+
+function cambiarGrisesMultiple(coleccion, value) {
+
+
+    for (var i = 0; i < coleccion.length; i++) {
+
+        coleccion[i].style.filter = 'grayscale(' + value + ')';
+    }
+}
+
+function actualizarSeleccionMenuDias() {
+    for (x = 0; x < DiasSemana.length; x++) {
+        asignarClass(DiasSemana[x].Valor, "MenuLateral text-center");
+    }
+    asignarClass(valorDia, "MenuLateralSeleccionado text-center");
+}
+
+
+function actualizarSeleccionCurso() {
+    var coleccion = document.getElementsByClassName('cursoDisponible');
+
+    for (var i = 0; i < coleccion.length; i++) {
+
+        coleccion[i].style.borderColor = 'rgba(0,0,0,.125)';
+    }
+    document.getElementById(valorCurso).style.borderColor = '#0366d6';
+}
+
+function limpiarSeleccionCurso() {
+    var coleccion = document.getElementsByClassName('cursoDisponible');
+
+    for (var i = 0; i < coleccion.length; i++) {
+
+        coleccion[i].style.borderColor = 'rgba(0,0,0,.125)';
+    }
+}
